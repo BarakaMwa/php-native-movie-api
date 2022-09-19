1e744424e72f8097df40cb03c3b006ee9a63c17b
baraka mwangamba <mwangamb@zra.org.zm>
2022-09-19 17:00:42 +0200

    TitleChange

--- navbar.php
+++ navbar.php
@@ -20,7 +20,7 @@
                 </span>
             <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-4"
                                                  href="https://mobirise.com">
-                        MOBIRISE
+                        MovieApp-Php
                     </a></span>
         </div>
     </div>
